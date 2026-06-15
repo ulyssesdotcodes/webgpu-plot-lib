c8e540da85291ecdd896a8bf1b2712e6477ba4ca
Ulysses Popple <ulysses@ulysses.codes>
2026-06-15 10:56:13 +0100

    set up toolchain

--- flake.nix
+++ flake.nix
@@ -4,30 +4,35 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import nixpkgs {inherit system;};
-      in {
-        devShells = with pkgs; {
-          default = mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            rustup
-            wasm-pack
-          ];
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+      rust = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+        targets = [ "wasm32-unknown-unknown" ];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          rust
+          wasm-pack
+          watchexec
+        ];
 
-          npmDeps = importNpmLock.buildNodeModules {
-            npmRoot = ./.;
-            inherit nodejs;
-          };
-
-          shellHook = ''
-            rustup target add wasm32-unknown-unknown
-            npx tsc --watch &
-            npx http-server  -c-1 public
-          '';
+        npmDeps = pkgs.importNpmLock.buildNodeModules {
+          npmRoot = ./.;
+          nodejs = pkgs.nodejs;
         };
       };
     });
